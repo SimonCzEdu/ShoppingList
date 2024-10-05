c52ded5806e0267426d7a2b9d36d58af0b262556
Szymon Czapiewski <simonczedu@gmail.com>
2024-10-05 15:17:01 +0000

    Create first draft view of Lists list

--- shopping_list/models.py
+++ shopping_list/models.py
@@ -8,7 +8,7 @@ class List(models.Model):
     list_name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="shopping_lists")
+    User, on_delete=models.CASCADE, related_name="shopping_list")
     
     def __str__(self):
         return f"{self.list_name}"
@@ -17,12 +17,15 @@ class Item(models.Model):
     list = models.ForeignKey(
         List, on_delete=models.CASCADE, related_name="items"
     )
-    item_name = models.CharField(max_length=200, unique=True)
+    item_name = models.CharField(max_length=200, unique=False)
     slug = models.SlugField(max_length=200, unique=True)
     min_quantity = models.IntegerField(default=0, blank=True)
     current_quantity = models.IntegerField(default=0, blank=True)
     priority = models.IntegerField(choices=PRIORITY, default=0)
     item_price = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
 
+    class Meta:
+        ordering = ['-list', 'item_name',]
+
     def __str__(self):
-        return f"{self.item_name}"
+        return f"{self.item_name} for {self.list}"
--- shopping_list/views.py
+++ shopping_list/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import List
 
 # Create your views here.
-def test(request):
-    return HttpResponse("Test")
+class ListsOfList(generic.ListView):
+    queryset = List.objects.all()
+    template_name = "lists_list.html"
