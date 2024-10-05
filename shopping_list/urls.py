39bb28a738dd192814c24eb9b90dc6ea23a45f2a
Szymon Czapiewski <simonczedu@gmail.com>
2024-10-05 18:54:46 +0000

    Add list_detail.html & link it up

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ListsOfList.as_view(), name='home'),
+    path('<slug:slug>/', views.list_detail, name='list_detail'),
 ]
--- shopping_list/views.py
+++ shopping_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import List
 
@@ -7,3 +7,14 @@ class ListsOfList(generic.ListView):
     queryset = List.objects.all()
     template_name = "shopping_list/index.html"
     paginate_by = 100
+
+def list_detail(request, slug):
+
+    queryset = List.objects.filter(slug=slug)
+    list = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "shopping_list/list_detail.html",
+        {"list": list},
+    )
