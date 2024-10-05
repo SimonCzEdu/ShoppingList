974393a67bd0c741bee0c60eca56744e4b220272
Szymon Czapiewski <simonczedu@gmail.com>
2024-10-05 04:39:05 +0000

    Add SECRET_KEY

--- planner/settings.py
+++ planner/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#9+6j1-z$o0y(cm^%(d23nx6mq-ofoo*gsrn6@skoqy84&8div'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
