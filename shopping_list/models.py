681a8bf524c4afa8a0d825550aa92561e1bc0379
Szymon Czapiewski <simonczedu@gmail.com>
2024-10-05 04:48:03 +0000

    Add List model

--- shopping_list/models.py
+++ shopping_list/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class List(models.Model):
+    list_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="blog_posts")
+    
+    def __str__(self):
+        return f"{self.list_name}"
