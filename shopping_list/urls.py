a693befafec0ef5637e3866ca295cbbcb5da7775
Szymon Czapiewski <simonczedu@gmail.com>
2024-10-06 08:44:55 +0000

    Fix for urls.py item_delete view

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path( '<slug:slug>/', views.list_detail, name='list_detail'),
     path( '<slug:slug>/edit_item/<int:item_id>',
         views.item_edit, name='item_edit'),
-    path('<slug:slug>/delete_item<int:item_id>',
+    path('<slug:slug>/delete_item/<int:item_id>',
         views.item_delete, name='item_delete'),
 ]
